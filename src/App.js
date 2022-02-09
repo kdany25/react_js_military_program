797955ad216bd09e7767b0d7a99c26826178799d
kdany25 <kabadany25@gmail.com>
2022-02-09 14:46:51 +0200

    ft:(Auth)

--- src/App.js
+++ src/App.js
@@ -11,40 +11,42 @@ import ContactUs from "./Pages/ContactUs/Contact";
 import Houses from "./Pages/House/House";
 import NewHouse from "./Pages/NewHouse/NewHouse";
 import HousePro from "./Pages/UpdateHouse/HousePro";
+import Login from "./Pages/Login/Login";
 function App() {
   return (
-    <Router>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/patients">
-            <PatientList />
-          </Route>
-          <Route path="/user/:userId">
-            <Patient />
-          </Route>
-          <Route path="/house/:Id">
-            <HousePro/>
-          </Route>
-          <Route path="/new">
-            <NewP />
-          </Route>
-          <Route path="/houses">
-            <Houses />
-          </Route>
-          <Route path="/newHouse">
-            <NewHouse />
-          </Route>
-          <Route path="/contact">
-            <ContactUs />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    // <Router>
+    //   <Topbar />
+    //   <div className="container">
+    //     <Sidebar />
+    //     <Switch>
+    //       <Route exact path="/">
+    //         <Home />
+    //       </Route>
+    //       <Route exact path="/patients">
+    //         <PatientList />
+    //       </Route>
+    //       <Route path="/user/:userId">
+    //         <Patient />
+    //       </Route>
+    //       <Route path="/house/:Id">
+    //         <HousePro/>
+    //       </Route>
+    //       <Route path="/new">
+    //         <NewP />
+    //       </Route>
+    //       <Route path="/houses">
+    //         <Houses />
+    //       </Route>
+    //       <Route path="/newHouse">
+    //         <NewHouse />
+    //       </Route>
+    //       <Route path="/contact">
+    //         <ContactUs />
+    //       </Route>
+    //     </Switch>
+    //   </div>
+    // </Router>
+    <Login />
   );
 }
 
