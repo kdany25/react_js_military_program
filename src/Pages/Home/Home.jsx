cb3fd9d79502099fb1975f4bf0672776039d4c0b
kdany25 <kabadany25@gmail.com>
2022-02-14 23:18:42 +0200

    ft:(insights)

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,14 +3,47 @@ import FeaturedInfo from "../../components/Insights/Insights";
 import "./home.css";
 import { userData } from "../../dummyData";
 import Daily from "../../components/daily/Daily";
-import Recent from "../../components/Recent/Recent";
+import { useEffect, useMemo, useState } from "react";
+import { userRequest } from "../../requestMethod";
+import Recent from "../../components/Recent/Recent"
 
 export default function Home() {
+  const [userStats, setUserStats] = useState([]);
+
+  const WEEKS = useMemo(
+    () => [
+     
+      "week 6",
+      "week 7",
+     
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get("/Health/week/patient");
+        res.data.map((item) =>
+          setUserStats((prev) => [
+            ...prev,
+            { name:WEEKS[item._id - 1], "Active User": item.total },
+          ])
+        );
+      } catch {}
+    };
+    getStats();
+  }, [WEEKS]);
   return (
     <div className="home">
       <FeaturedInfo />
       <div className="homec">
-      <Chart data={userData} title="Action Analytics" grid dataKey="Active User"/>
+      <Chart
+        data={userStats}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
       <Daily/>
       </div>
     </div>
--- src/components/Insights/Insights.jsx
+++ src/components/Insights/Insights.jsx
@@ -8,6 +8,17 @@ export default function Insights() {
   const [perc, setPerc] = useState(0);
   const [count, setCount] = useState();
 
+
+  const [incomeH, setIncomeH] = useState([]);
+  const [percH, setPercH] = useState(0);
+  const [countH, setCountH] = useState();
+
+  const [incomeR, setIncomeR] = useState([]);
+  const [percR, setPercR] = useState(0);
+  const [countR, setCountR] = useState();
+
+
+
   useEffect(() => {
     const getCount = async () => {
       try {
@@ -23,15 +34,64 @@ export default function Insights() {
   useEffect(() => {
     const getIncome = async () => {
       try {
-        const count = await userRequest.get("Health/count/all")
         const res = await userRequest.get("Health/week/patient");
-        setIncome(res.data);
-        setPerc((res.data[1].total * 100) / res.data[0].total - 100);
+        const ress = res.data.sort((a, b) => (a._id < b._id) ? 1 : -1)
+        setIncome(ress);
+        setPerc((ress[0].total * 100) / ress[1].total - 100);
       } catch {}
     };
     getIncome();
   }, []);
 
+  useEffect(() => {
+    const getCountH = async () => {
+      try {
+        const count= await userRequest.get("house/count/all")
+        setCountH(count.data);
+      } catch {}
+    };
+    getCountH();
+  }, []);
+
+
+  
+  useEffect(() => {
+    const getIncomeH = async () => {
+      try {
+        const res = await userRequest.get("house/week/patient");
+        const ress = res.data.sort((a, b) => (a._id < b._id) ? 1 : -1)
+        setIncomeH(ress);
+        setPercH((ress[0].total * 100) / ress[1].total - 100);
+      } catch {}
+    };
+    getIncomeH();
+  }, []);
+
+
+
+  useEffect(() => {
+    const getCountR = async () => {
+      try {
+        const count= await userRequest.get("roads/week/sum")
+        setCountR(count.data[0].total);
+      } catch {}
+    };
+    getCountR();
+  }, []);
+
+  
+  useEffect(() => {
+    const getIncomeR = async () => {
+      try {
+        const res = await userRequest.get("roads/week/patient");
+        const ress = res.data.sort((a, b) => (a._id < b._id) ? 1 : -1)
+        setIncomeR(ress);
+        setPercR((ress[0].total * 100) / ress[1].total - 100); ;
+    
+      } catch {}
+    };
+    getIncomeR();
+  }, []);
 
   return (
     <div className="featured">
@@ -45,7 +105,7 @@ export default function Insights() {
         <div className="featuredMoneyContainer">
           <span className="featuredMoney">{count}</span>
           <span className="featuredMoneyRate">
-          %{Math.floor(perc)}{" "}
+          {Math.floor(perc)}{"%"}
             {perc < 0 ? (
               <ArrowDownward className="featuredIcon negative" />
             ) : (
@@ -63,9 +123,14 @@ export default function Insights() {
           </div>
         </div>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">24</span>
+          <span className="featuredMoney">{countH}</span>
           <span className="featuredMoneyRate">
-            -2 <ArrowDownward className="featuredIcon negative" />
+             {Math.floor(percH)}{"%"}
+            {percH < 0 ? (
+              <ArrowDownward className="featuredIcon negative" />
+            ) : (
+              <ArrowUpward className="featuredIcon" />
+            )}
           </span>
         </div>
         <span className="featuredSub">Compared to last week</span>
@@ -78,9 +143,14 @@ export default function Insights() {
           </div>
         </div>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">18Km</span>
+          <span className="featuredMoney">{countR}Km</span>
           <span className="featuredMoneyRate">
-            +2.4 <ArrowUpward className="featuredIcon" />
+          {Math.floor(percR)}{"%"}
+            {percR < 0 ? (
+              <ArrowDownward className="featuredIcon negative" />
+            ) : (
+              <ArrowUpward className="featuredIcon" />
+            )}
           </span>
         </div>
         <span className="featuredSub">Compared to last week</span>
